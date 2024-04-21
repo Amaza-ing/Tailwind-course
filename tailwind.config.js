b14e2772cc0622b82f18a1b056941ed4e59a2979
Adrián Maza <adri_mv1@hotmail.com>
2024-04-21 14:54:42 +0200

    customisation colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#2f3044",
+      },
+      backgroundImage: {
+        circles: "url('./assets/bg-pattern-circles.svg')",
+        intro: "url('./assets/bg-pattern-intro-desktop.svg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
